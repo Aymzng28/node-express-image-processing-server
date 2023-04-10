6454c1530ea136164b6e331e823c8bc262cb8488
Yume28 <steevensept09@gmail.com>
2023-04-10 17:30:27 +0800

    commit

---
+++ api/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
+
+app.use('/*', (request, response) => {
+   response.sendFile(pathToIndex); 
+});
+
+module.exports = app;  
